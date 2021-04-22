8bd83fbf037102d53fa58bd0d7c77c90c3c168e8
Ava Cutler <82968143+avacutler@users.noreply.github.com>
2021-04-22 17:04:19 -0500

    Update navbar.js
    hamburger

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("button").click(function(){
 
       if($("button").text() == "☰"){
-        $("button").text("🞬");
+        $("button").text("☰");
       }else{
         $("button").text("☰");
       }
